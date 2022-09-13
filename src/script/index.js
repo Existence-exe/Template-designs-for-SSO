898cd3990651fec6667d866dfee0ce59fcf3428b
pd <pritishdutta20@gmail.com>
2022-09-13 10:16:11 +0530

    nav-element

---
+++ src/script/index.js
@@ -0,0 +1,7 @@
+home_button = document.getElementById('nav-home')
+members_button = document.getElementById('nav-members')
+gallery_button = document.getElementById('nav-gallery')
+
+function locate(location) {
+    window.location.href = location
+}
