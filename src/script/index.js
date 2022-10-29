eeea59222a28bae2ac88ff1e45b7550e2ca08bee
Existence.exe <pritishdutta20@gmail.com>
2022-10-30 01:00:23 +0530

    mobile view basic finished

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,64 @@
 const hamburger = document.querySelector('.mobile-nav-hamburger');
 const tabs = document.querySelector('.mobile-nav-tabs');
+const hamburgers = document.querySelector('.hamburgers')
+const tab = Array.from(document.getElementsByClassName('mobile-nav-tab'));
+const contact_us = document.querySelector('.mobile-contact-us')
+const classes = tabs.classList
+const cancel_button = document.querySelector('.mobile-nav-cancel')
+hamburgers.addEventListener('click', () => {
+    if (classes.contains('passive')) {
+        classes.remove('passive')
+        tabs.setAttribute('style', `
+        position:absolute;
+        display:flex;
+        flex-direction: column;
+        justify-content : space-around;
+        align-items : flex-start;
+        right:  -3vh;
+        top : 0;
+        height : 100vh ;
+        background-color : 	hsl(175 14% 82% / 0.1);
+        backdrop-filter: blur(1rem);
+        border-radius : 2vh;
+        z-index : +1;
+        width:80vw;
+        'transition: width 0.25s ease-in-out,height 0.25s ease-in-out;'
+        `
+        )
+        tab.forEach(e => {
+            e.setAttribute('style', 'display:block')
+        });
+        contact_us.setAttribute('style', 'display:block')
+        cancel_button.setAttribute('style', 'display:flex')
+        classes.add('flex')
+        classes.add('active')
+        hamburgers.setAttribute('style', 'display : none')
+        hamburger.setAttribute('style', `
+        width : 0 ;
+        height : 0  ;
+        `
+        )
+    } else {
+        
+    }
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('nav-mobile-body-active');
-    tabs.classList.toggle('mobile-tab-active');
 })
+
+cancel_button.addEventListener('click', () => {
+    if(classes.contains('active')){ 
+        tabs.setAttribute('style', '')
+        classes.remove('flex')
+        classes.remove('active')
+        classes.add('passive')
+        hamburger.setAttribute('style', ``)
+        tab.forEach(e => {
+            e.setAttribute('style', 'display:none')
+        });
+        contact_us.setAttribute('style', 'display:none')
+        hamburgers.setAttribute('style', 'display : flex')
+        cancel_button.setAttribute('style', 'display:none')
+        console.log(tabs.attributes)
+    }
+})
+
+
